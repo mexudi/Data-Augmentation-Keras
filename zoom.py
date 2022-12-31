b19f68a492e8ae7e1fa33c4606e10a481b23483d
mexudi <soufianmexudi20@gmail.com>
2022-12-31 12:46:02 +0100

    zoom technique

---
+++ zoom.py
@@ -0,0 +1,15 @@
+%matplotlib inline
+
+import os
+import numpy as np
+import tensorflow as tf
+
+from PIL import Image
+from matplotlib import pyplot as plt
+
+print('Using TensorFlow', tf.__version__)
+
+generator = tf.keras.preprocessing.image.ImageDataGenerator(zoom_range = [0.5,2])
+
+x, y = next(generator.flow_from_directory('images', batch_size=1))
+plt.imshow(x[0].astype('uint8'));
